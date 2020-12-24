cbc950c82d31774ebf9bfa8741bedede3bc312c4
boykovm <66530211+boykovm@users.noreply.github.com>
2020-12-25 00:00:58 +0200

    Update index.js

--- keys/index.js
+++ keys/index.js
@@ -1,12 +1,12 @@
-// if (process.env.NODE_ENV === 'production') {
-//     module.exports = require('./keys.prod')
-// } else {
-//     module.exports = require('./keys.dev')
-// }
-module.exports = {
-    MONGODB_URI: `mongodb+srv://admin:123@cluster0.bbjw8.mongodb.net/shop?retryWrites=true&w=majority`,
-    SESSION_SECRET: 'some secret value',
-    // SENDGRID_API_KEY: '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',
-    // EMAIL_FROM: 'mococik578@95ta.com',
-    BASE_URL: 'http://localhost:3000'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
+// module.exports = {
+//     MONGODB_URI: `mongodb+srv://admin:123@cluster0.bbjw8.mongodb.net/shop?retryWrites=true&w=majority`,
+//     SESSION_SECRET: 'some secret value',
+//     // SENDGRID_API_KEY: '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',
+//     // EMAIL_FROM: 'mococik578@95ta.com',
+//     BASE_URL: 'http://localhost:3000'
+// }
